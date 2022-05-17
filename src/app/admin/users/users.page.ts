5904b0ef6fdea3a5bca3fdc03c6eb0c45ae217c8
joptimus <jlewan27@gmail.com>
2022-05-17 15:47:10 -0500

    Added Options page

--- src/app/admin/users/users.page.ts
+++ src/app/admin/users/users.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersPage implements OnInit {
 
-  constructor() { }
+  credentials: FormGroup;
+
+  constructor(
+    private authService: AuthenticationService, 
+    private loadingController: LoadingController, 
+    private fb: FormBuilder, 
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  get email() {
+    return this.credentials.get('email');
+  }
+  get password() {
+    return this.credentials.get('password');
   }
 
+  async register() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+
+    const user = await this.authService.register(this.credentials.value);
+    await loading.dismiss();
+
+    if (user) {
+      this.showAlert('Success','User Successfully added');
+    } else {
+      this.showAlert('Registration Failed', 'Please try again');
+    }
+  }
+
+  async showAlert(header, message) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
